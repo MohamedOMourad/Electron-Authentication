a7c238374ea0e7aef470f7b9f6a1febf3e1216e0
user <user@users-iMac.local>
2023-01-19 09:39:22 +0200

    first commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,17 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Hello = () => {
+  return <div>ewwe</div>;
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
